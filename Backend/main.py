0143d79c1346e53db07fb301022236789d98281c
Sam Fulham <samfulham12@gmail.com>
2026-03-25 18:18:15 +0000

    added more comments

--- Backend/main.py
+++ Backend/main.py
@@ -11,11 +11,13 @@ load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
 
 api_key = os.getenv("OPENAI_API_KEY")
 
+#just here so i can test if it found
 if not api_key:
     raise ValueError("OPENAI_API_KEY not found. Check your enviorment variables")
 
 client = OpenAI(api_key=api_key)
 
+#just allowing everything for now might change latet
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -25,6 +27,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+#only accepting lists if strings so it expects [text 1, text 2, text 3 ....]
 class TextUploadRequest(BaseModel):
     texts: List[str]
 
@@ -32,10 +35,12 @@ class GenerateRequest(BaseModel):
     prompt: str
     texts: List[str]
 
+#test to see if backend is up 
 @app.get("/")
 def read_root():
     return {"message": "Backend is running"}
 
+#getting all functions
 @app.post("/add_texts")
 def add_texts(request: TextUploadRequest):
     return update_profile(request.texts)
@@ -54,19 +59,17 @@ def profile_from_texts(request: TextUploadRequest):
 def clear_profile():
     return reset_profile()
 
+#where the prompt is built it basiclly appendes your prompt with your stylesheet
 def build_style_prompt(profile: dict, user_prompt: str) -> str:
     return f"""
 Write in the following style:
-
 Average sentence length: {profile.get("avg_sentence_length")}
 Average word length: {profile.get("avg_word_length")}
 Average paragraph length: {profile.get("avg_paragraph_length")}
 Vocabulary richness: {profile.get("vocabulary_richness")}
 Readability score: {profile.get("avg_readability")}
-
 Common words: {profile.get("top_10_words")}
 Common bigrams: {profile.get("top_10_bigrams")}
-
 Now write about:
 {user_prompt}
 """
@@ -86,6 +89,7 @@ async def generate_text(request: GenerateRequest):
                 {"role": "system", "content": "You imitate writing style."},
                 {"role": "user", "content": style_prompt}
             ],
+            #crontols how creative the model is
             temperature=0.8
         )
 
@@ -93,5 +97,6 @@ async def generate_text(request: GenerateRequest):
             "generated_text": response.choices[0].message.content
         }
 
+    #here incase anything goes wrong
     except Exception as e:
         return {"error": str(e)}
